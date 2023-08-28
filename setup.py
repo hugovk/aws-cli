60bbbc297560c1ffe090993bba095f7c59770759
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-08-28 19:53:59 +0300

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup_options = dict(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     project_urls={
         'Source': 'https://github.com/aws/aws-cli',
